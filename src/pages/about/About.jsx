800065593c8341e17add94af1ca2d0570f5b352a
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2023-05-19 23:12:32 +0200

    mise a jour

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import "./about.css";
+import photo from "../../assets/images/photo.png"
 
 const About = () => {
   return (
     <div className="container_about">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi pariatur
-        ad optio numquam porro nam hic corporis error, nesciunt voluptatum
-        doloremque officia libero perspiciatis, deleniti voluptas sapiente modi
-        explicabo beatae? ABOUT
-      </p>
+      <div className="content_about">
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi pariatur
+          ad optio numquam porro nam hic corporis error, nesciunt voluptatum
+          doloremque officia libero perspiciatis, deleniti voluptas sapiente
+          modi explicabo beatae? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eligendi consectetur blanditiis earum unde laborum,
+          molestias eum commodi similique accusamus. Tempore rerum maxime
+          quaerat laboriosam hic, ipsum possimus. Dolor, iste illo! Lorem ipsum
+          dolor sit amet consectetur, adipisicing elit. Et quia eveniet ducimus
+          neque cum. Nam blanditiis excepturi velit, modi minus, accusamus ex
+          illo incidunt animi dolorem exercitationem, provident rem quo? ABOUT
+        </p>
+      </div>
+      <div className="content_profil">
+        <div className="content_name">
+          <div className="content_download">
+            <h2>Benjamin EKIA MOUANGA</h2>
+            <button className="content_btn">Download CV</button>
+          </div>
+        </div>
+        <div className="content_photo">
+          <img src={photo} className="content_img"  height="100%"/>
+        </div>
+      </div>
     </div>
   );
 };
