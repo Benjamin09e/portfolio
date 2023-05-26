1e60d226349e5053d6cdca26640a13e9aa2950d8
Benjamin EKIA <ekiabenjamin@gmail.com>
2023-05-26 22:39:20 +0200

    mise a jour

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -81,37 +81,6 @@ const Skills = () => {
           </p>
         </div>
       </div>
-      <div className="skills_elements">
-        <div className="skills_parents">
-          <div className="skills_group">
-            <div className="group_left">
-              <h3>Form</h3>
-              <p>Lorem</p>
-            </div>
-            <div className="group_right">
-              <h3>To</h3>
-              <p>Lorem</p>
-            </div>
-          </div>
-          <div className="skills_bottom">
-            <h3>Company</h3>
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="skills_descriptions">
-          <h3>Experiences</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-            pariatur ad optio numquam porro nam hic corporis error, nesciunt
-            voluptatum doloremque officia libero perspiciatis, deleniti voluptas
-            sapiente modi explicabo beatae? SKILLS
-          </p>
-        </div>
-      </div>
       
     </div>
   );
